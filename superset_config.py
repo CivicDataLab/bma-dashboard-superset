4dfbd09ab8d67323d1b592ef0b643e5f5d22d81b
bhavabhuthi <bhavabhuthi@riseup.net>
2025-01-28 17:57:58 +0530

    fix the csrf exempt list variable

--- superset_config.py
+++ superset_config.py
@@ -13,7 +13,7 @@ SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = True
 # Add endpoints that need to be exempt from CSRF protection
-WTF_CSRF_EXEMPT_LIST = os.getenv('CSRF_EXEMPT_LIST')
+WTF_CSRF_EXEMPT_LIST = [os.getenv('CSRF_EXEMPT_LIST')]
 # A CSRF token that expires in 1 year
 WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
 
